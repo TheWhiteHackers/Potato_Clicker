e410f5354892ae505d465d74bd94bebee2c57d16
TheWhiteHackers <nurefsanbera@gmail.com>
2023-07-06 15:32:35 -0500

    increase

--- js.js
+++ js.js
@@ -1,7 +1,15 @@
+let counter = -1;
 window.onload=start();
 function start(){
     increasescore()
 }
-function increasescore(){
-    const thescore = document.getElementsById("scorecon");
+
+function increasescore(id, inc){
+    const thescore = document.getElementById("scorecon");
+    
+    counter++
+    console.log(counter);
+
+    thescore.innerHTML="$"+counter;
+
 }
