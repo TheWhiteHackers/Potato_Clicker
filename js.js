c1801fee5f86e67e43313012426d7cebfac0e264
TheWhiteHackers <nurefsanbera@gmail.com>
2023-07-10 11:53:32 -0500

    still going...

--- js.js
+++ js.js
@@ -14,6 +14,19 @@ function increasescore(id, inc){
 
 }
 
-function challed(){
-    
+function farmer(){
+    const thescore = document.getElementById("scorecon");
+    const farmerval = document.getElementById("farmervalue");
+    console.log(thescore.value);
+    console.log(farmerval.value);
+
+    if (thescore.value === farmerval.value){
+        counter = counter + 2
+        console.log(counter);
+    } else {
+        alert("you dont have enough money")
+    }
+   
+
+    thescore.innerHTML="$"+counter;
 }
