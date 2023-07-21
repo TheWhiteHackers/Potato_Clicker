244e61955b8d5f44122541ab3cc85ec060b24d83
TheWhiteHackers <nurefsanbera@gmail.com>
2023-07-21 14:48:33 -0500

    trying autoclick....

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 let buylist = [
     ["farmer", 10, 1, "farmerval"],
-    ["animals", 15, 0, "animalval"],
+    ["animals", 15, 0.5, "animalval"],
     ["farm", 100, 5, "farmval"],
-    ["farm2", 350, 0, "farm2val"]
+    ["farm2", 350, 1, "farm2val"]
 ]
 let powerlist = [
     ["rebirth", 1000000],
@@ -11,7 +11,8 @@ let powerlist = [
 ]
 let counter = -1;
 let incval = 1;
-let autorate = 0;
+let autocounter = 0;
+let autoincval = 0;
 window.onload=start();
 function start(){
     increasescore()
@@ -21,7 +22,6 @@ function start(){
     document.getElementById("credbox").style.display='none';
     document.getElementById("soyousteal").style.display='none';
 }
-
 function increasescore(id, inc){
 //money
     const thescore = document.getElementById("finalscore");
@@ -31,13 +31,13 @@ function increasescore(id, inc){
 
 //auto
     const autoscore = document.getElementById("scoreauto");
-    autoscore.innerHTML="Auto: "+autorate+" per sec";
+    
+    autoscore.innerHTML="Auto: "+autocounter+" per sec";
 }
 
 function buyitem(id){
     const thescore = document.getElementById("finalscore");
     console.log(buylist[id][1]);
-
 
     if (counter >= buylist[id][1]){
         incval += buylist[id][2];
@@ -47,26 +47,33 @@ function buyitem(id){
         console.log(counter);
     } else {
         document.getElementById("nomoney").style.display='block';
-        }
+    }
    
     document.getElementById("incvalscreen").innerHTML="Clicker Value: "+incval;
     thescore.innerHTML="$"+counter;
 }
+//--------------------------------------
 function autoclick(id){
 
     if(counter >= buylist[id][1]){
-        autorate += buylist[id][2];
-       // buylist[id][1] += 5;
+        autoincval += buylist[id][2];
+        counter = counter-buylist[id][1];
+        buylist[id][1] += 5;
+        document.getElementById(buylist[id][3]).innerHTML="$"+buylist[id][1];
     } else {
-        alert("no money");
+        document.getElementById("nomoney").style.display='block';
     }
+
+    console.log("SUP");
+    document.getElementById("scoreauto").innerHTML="Auto: "+autoincval+" per sec";
 }
+//-----------------------------------------
 function closebtn(){
     document.getElementById("nomoney").style.display='none';
     document.getElementById("shadow").style.display='none';
-    document.getElementById("shadow").style.display='none';
     document.getElementById("challcon").style.display='none';
     document.getElementById("credbox").style.display='none';
+    document.getElementById("soyousteal").style.display='none';
 }
 function challed(){
     document.getElementById("shadow").style.display='block';
@@ -77,6 +84,7 @@ function opencred(){
     document.getElementById("credbox").style.display='block';
 }
 function opensteal(){
+    document.getElementById("nomoney").style.display='none';
     document.getElementById("shadow").style.display='block';
-    document.getElementById("steal").style.display='block';
+    document.getElementById("soyousteal").style.display='block';
 }
